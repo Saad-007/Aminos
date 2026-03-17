4c97d103019700086aa20f363326e911584cf0c1
Saad-007 <saadsafeer223@gmail.com>
2026-03-17 11:55:54 +0500

    upload task

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from "react";
+import Home from "./components/Home";
+import "./App.css"; // Keep this to ensure your Tailwind CSS applies
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
